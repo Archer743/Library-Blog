97337fed169a143ee3885a4b04b4f7f8fdd76313
Farfoleta175 <127017147+Farfoleta175@users.noreply.github.com>
2023-03-05 13:32:34 +0200

    Update database.sql
    [UPDATE] добавих primary key в fav_books

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
-CREATE TABLE users(
-id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE DATABASE library;
+USE library;
+CREATE TABLE IF NOT EXISTS users(
+user_id INT PRIMARY KEY AUTO_INCREMENT,
 username VARCHAR(50) NOT NULL,
 email_address VARCHAR(100) NOT NULL UNIQUE,
 country VARCHAR(50) DEFAULT 'Unknown',
@@ -8,19 +10,19 @@ gender ENUM('male', 'female', 'other'),
 status ENUM('student', 'teacher', 'other')
 );
 
-CREATE TABLE admins(
+CREATE TABLE IF NOT EXISTS admins(
 admin_id INT,
 CONSTRAINT FK_admins_users FOREIGN KEY(admin_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE experts(
+CREATE TABLE IF NOT EXISTS experts(
 expert_id INT,
 CONSTRAINT FK_experts_users FOREIGN KEY(expert_id) REFERENCES users(user_id)
 );
-
-CREATE TABLE liked_books(
-user_id INT NOT NULL,
-book_id INT NOT NULL,
-CONSTRAINT FK_liked_books_users FOREIGN KEY(user_id) REFERENCES users(user_id),
-CONSTRAINT FK_liked_books_books FOREIGN KEY(book_id) REFERENCES books(id)
+CREATE TABLE IF NOT EXISTS fav_books (
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    CONSTRAINT FK_fav_books_users FOREIGN KEY(user_id) REFERENCES users(id),
+    CONSTRAINT FK_fav_books_books FOREIGN KEY(book_id) REFERENCES books(id),
+    CONSTRAINT PK_fav_books PRIMARY KEY (user_id, book_id)
 );
